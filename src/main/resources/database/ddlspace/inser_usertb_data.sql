2ea140b6731c07e33d19a2db121036dff3c270f2
LIZUANMASTER\Lizuan <mayahate77@gmail.com>
2023-09-03 23:41:30 +0800

    [Update] 合併兩個小專案，套用前端模板調整程式內容

--- src/main/resources/database/ddlspace/inser_usertb_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO [lizdb].[dbo].[usertb]([username]
-      ,[password]
-      ,[enabled]
-      ,[accountnonexpired]
-      ,[accountnonlocked]
-      ,[credentialsnonexpired]) VALUES('lizuan','{bcrypt}$2a$10$LDqK0ms6BbW3/9nGcGHTi.AnROGIl2GBUlwGQ8jufBM8Er1IpfTGi',1,1,1,1)
